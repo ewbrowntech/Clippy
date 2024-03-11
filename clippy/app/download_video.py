808ec65896d9d0395883adba2960d46ee8501521
Ethan Brown <ethan@ewbrowntech.com>
2024-03-11 01:36:19 -0500

    Set up framework for downloading YouTube videos

--- clippy/app/download_video.py
+++ clippy/app/download_video.py
@@ -10,11 +10,65 @@ All rights reserved. This file is part of the Clippy project and is released und
 the MIT License. See the LICENSE file for more details.
 """
 
+import discord
+from discord import Message
 
-async def download_video(message):
-    if message.content.startswith("!download-video-only"):
-        await message.channel.send("Download a video (video only)")
-    elif message.content.startswith("!download-audio"):
-        await message.channel.send("Download a video (audio)")
+
+async def download_video(message: Message):
+    """
+    Run the download operation
+    """
+    # Extract the URL
+    video_id = await extract_video_id(message.content)
+
+    if message.content.startswith("!download-audio"):
+        response = await message.channel.send(
+            "Download a video (audio)", reference=message
+        )
+        # Download the audio stream
+        response = await download_audio_stream(video_id, response)
+        # Upload the audio stream
+
+    elif message.content.startswith("!download-video-only"):
+        response = await message.channel.send(
+            "Download a video (video only)", reference=message
+        )
+        # Get the available resolutions
+        # Download video stream
+        response = await download_audio_stream(video_id, response)
+        # Upload the video stream
+
     elif message.content.startswith("!download-video"):
-        await message.channel.send("Download a video (multimedia)")
+        await message.channel.send("Download a video (multimedia)", reference=message)
+        # Get the available resolutions
+        # Download the audio stream
+        response = await download_audio_stream(video_id, response)
+        # Download the video stream
+        response = await download_video_stream(video_id, response)
+        # Merge the audio and video streams
+        response = await merge_multimedia(response)
+        # Upload the multimedia file
+
+    return
+
+
+async def extract_video_id(message_content: str):
+    video_id = None
+    return video_id
+
+
+async def download_audio_stream(video_id: str, response: Message):
+    response = await response.edit(content="Downloading the audio stream...")
+    return response
+
+
+async def download_video_stream(video_id: str, response: Message):
+    response = await response.edit(content="Downloading the video stream...")
+    return response
+
+
+async def merge_multimedia(response: Message):
+    response = await response.edit(
+        content="Merging the audio and video stream... (This may take a while)"
+    )
+    return response
